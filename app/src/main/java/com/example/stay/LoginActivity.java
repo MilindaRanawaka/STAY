e3f7100396ecbd4e4cc4623ffa97658586d9e8d9
MilindaRanawaka <milindaranawaka@gmail.com>
2019-07-31 22:55:52 +0530

    Login UI Finish

--- app/src/main/java/com/example/stay/LoginActivity.java
+++ app/src/main/java/com/example/stay/LoginActivity.java
@@ -3,12 +3,32 @@ package com.example.stay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tv1,tv2;
+    String str1,str2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin(View view){
+        tv1 = findViewById(R.id.unText);
+        tv2 = findViewById(R.id.pwdText);
+
+        str1 = tv1.getText().toString();
+        str2 = tv2.getText().toString();
+
+        if((str1 == "1")&&(str2 == "1")){
+            tv1.setText("ADMIN");
+        } else if((str1 == "2")&&(str2 == "2")){
+            tv1.setText("User");
+        }
+
     }
 }
