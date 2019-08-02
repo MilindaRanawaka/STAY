f11256a12b5969f71aa7773c1de441a3aea315ca
MilindaRanawaka <milindaranawaka@gmail.com>
2019-08-02 20:53:32 +0530

    Add Boarder Done

--- app/src/main/java/com/example/stay/ManagementHomeActivity.java
+++ app/src/main/java/com/example/stay/ManagementHomeActivity.java
@@ -2,7 +2,9 @@ package com.example.stay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ManagementHomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ManagementHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management_home);
+    }
+
+    public void onBorder(View view){
+        Intent intent = new Intent(this,MngBorderActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/stay/ui/main/Frag1.java
@@ -0,0 +1,27 @@
+package com.example.stay.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.stay.R;
+
+public class Frag1 extends Fragment {
+
+    EditText tv1;
+    String str1;
+    int num1;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_boarder_layout, container, false);
+    }
+
+}
