9e56475d1823e0b0863dd5f3ae1fe4ffb973b03e
MalshaNanayakkara <malshananayakkara20@gmail.com>
2019-08-03 23:31:39 +0530

    malsha

---
+++ app/src/main/java/com/example/stay/MngFoodRequest.java
@@ -0,0 +1,16 @@
+package com.example.stay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MngFoodRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mng_food_request);
+    }
+}
+//haiyooo
+//mata ba
