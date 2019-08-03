18dacea2b51dde46b49dab9dace3193cd81c7cee
ransa <ransararp4@gmail.com>
2019-08-04 00:45:08 +0530

    Leave Request Done

--- app/src/main/java/com/example/stay/BoarderLeaveHomeActivity.java
+++ app/src/main/java/com/example/stay/BoarderLeaveHomeActivity.java
@@ -2,7 +2,9 @@ package com.example.stay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BoarderLeaveHomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BoarderLeaveHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boarder_leave_home);
+    }
+
+    public void onEarlyLeaveReq(View view) {
+        Intent intent = new Intent(this,BoarderLeaveRequest.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/stay/BoarderLeaveRequest.java
@@ -0,0 +1,15 @@
+package com.example.stay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class BoarderLeaveRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boarder_leave_request);
+    }
+}
