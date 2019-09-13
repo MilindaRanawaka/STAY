6c4ad6da682739534d58dc4f92bc727895b470fd
ransa <ransa@DESKTOP-F01AG7U>
2019-09-13 15:11:50 +0530

    Add leave

---
+++ app/src/main/java/Database/LeaveReq.java
@@ -0,0 +1,88 @@
+package Database;
+
+public class LeaveReq {
+    private  String key;
+    private String Name;
+    private String roomNo;
+    private String Reason;
+    private String leaveType;
+    private String leaveDate;
+    private String leaveTime;
+    private String arivalDate;
+    private String arivalTime;
+
+    public LeaveReq() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(String roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public String getReason() {
+        return Reason;
+    }
+
+    public void setReason(String reason) {
+        Reason = reason;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public String getLeaveDate() {
+        return leaveDate;
+    }
+
+    public void setLeaveDate(String leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+
+    public String getLeaveTime() {
+        return leaveTime;
+    }
+
+    public void setLeaveTime(String leaveTime) {
+        this.leaveTime = leaveTime;
+    }
+
+    public String getArivalDate() {
+        return arivalDate;
+    }
+
+    public void setArivalDate(String arivalDate) {
+        this.arivalDate = arivalDate;
+    }
+
+    public String getArivalTime() {
+        return arivalTime;
+    }
+
+    public void setArivalTime(String arivalTime) {
+        this.arivalTime = arivalTime;
+    }
+}
--- app/src/main/java/com/example/stay/BoarderLeaveRequest.java
+++ app/src/main/java/com/example/stay/BoarderLeaveRequest.java
@@ -1,15 +1,67 @@
 package com.example.stay;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import Database.LeaveReq;
 
 public class BoarderLeaveRequest extends AppCompatActivity {
 
+    EditText boarderName,roomNo,reason,leaveDate,leaveTime,arrivalDate,arivalTime;
+    Button addBtn;
+    DatabaseReference dbRef;
+    LeaveReq leave;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boarder_leave_request);
+
+        boarderName = findViewById(R.id.boarderTxt);
+        roomNo = findViewById(R.id.roomNoTxt);
+        reason = findViewById(R.id.reasonTxt);
+        leaveDate = findViewById(R.id.leaveDateTxt);
+        leaveTime = findViewById(R.id.leaveTimeTxt);
+        arrivalDate = findViewById(R.id.arivalDateTxt);
+        arivalTime = findViewById(R.id.arivalTimeTxt);
+
+        addBtn = findViewById(R.id.addBtnAddLeave);
+        leave = new LeaveReq();
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef = FirebaseDatabase.getInstance().getReference("LeaveRequest");
+                leave.setKey(dbRef.push().getKey());
+                leave.setName(boarderName.getText().toString());
+                leave.setRoomNo(roomNo.getText().toString());
+                leave.setReason(reason.getText().toString());
+                leave.setLeaveDate(leaveDate.getText().toString());
+                leave.setLeaveTime(leaveTime.getText().toString());
+                leave.setArivalDate(arrivalDate.getText().toString());
+                leave.setArivalTime(arivalTime.getText().toString());
+
+                dbRef.child(leave.getKey()).setValue(leave);
+
+                Toast.makeText(BoarderLeaveRequest.this,"Data Added Succesfully",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
